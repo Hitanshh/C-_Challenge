1dffa0b96672bedb00fb55024cfdc6dc29d71dc0
Hitansh <hitanshdubey225@gmail.com>
2024-11-14 02:12:07 +0530

    DAY01

--- DAY-01--Printing/DiamondPattern.cpp
+++ DAY-01--Printing/DiamondPattern.cpp
@@ -2,15 +2,41 @@
 using namespace std;
 
 void DiamondPattern(int n){
-    for (int i = 0; i < n*2; i++)
+    for (int i = 0; i < n; i++)
     {
 
+        for (int j = 0; j <n-i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 0; j <i*2+1; j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 0; j <n-i+1; j++)
+        {
+            cout<<"  ";
+        }
         
-
-
-
         cout<<endl;
     }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 0; j < n*2-(2*i+1); j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
     
 }
 

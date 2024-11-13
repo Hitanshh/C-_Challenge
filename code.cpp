b33111e39ec5faa2b080838e8b42e31ac2bd91ec
Hitansh <hitanshdubey225@gmail.com>
2024-11-13 14:19:25 +0530

    SquarePrint

--- code.cpp
+++ code.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-using namespace 
-int main(){
+using namespace std;
+
+void PrintSquare(int n){
+    for (int i = 0; i < n ; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
     
+}
+
+
+
+int main(){
+    PrintSquare(5);
+    return 0;
 }
